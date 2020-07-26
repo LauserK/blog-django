2ee4f29d406d739fa9389fc38782648002ff428c
Kildare Lauser <kildarealauser@gmail.com>
2020-07-26 15:03:11 -0400

    Comments working

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import View
-from .models import Article
+from .models import Article, Comment
 
 class HomeView(View):
     def get(self, request):
@@ -18,6 +18,33 @@ class SinglePostView(View):
 
         
 
+        template_name = "single.html"
+        context = {
+            "article": article,
+            "comments": comments
+        }
+        return render(request, template_name, context)
+
+    def post(self, request, slug):
+        article = get_object_or_404(Article, slug=slug)
+        comments = article.comments.filter(parent__isnull=True)
+
+        id_parent = request.POST.get('span-answer')
+        comment_body = request.POST.get('body')
+        
+        if (id_parent and comment_body) and (id_parent != '' and comment_body != ''):
+            comment = Comment()
+            comment.author = request.user
+            comment.content = comment_body            
+
+            if id_parent != 'post':
+                comment.parent = Comment.objects.get(pk=id_parent)
+
+            comment.save()
+
+            if id_parent == 'post':
+                article.comments.add(comment)
+        
         template_name = "single.html"
         context = {
             "article": article,
--- public/static/js/comments.js
+++ public/static/js/comments.js
@@ -1,4 +1,10 @@
+function getCookieValue(a) {
+  var b = document.cookie.match("(^|;)\\s*" + a + "\\s*=\\s*([^;]+)");
+  return b ? b.pop() : "";
+}
+
 function changeObjective(e) {
+  /*
   var hidden = document.getElementsByName("span-answer");
   var span = document.getElementById("span-answer");
   var button = document.querySelector(".remove-comment");
@@ -8,9 +14,22 @@ function changeObjective(e) {
     button.classList.toggle("activado");
   }
 
-  var x = e.target.dataset.pk;
+  
   hidden.value = x;
-  span.innerHTML = "#" + x;
+  span.innerHTML = "#" + x;*/
+
+  e.preventDefault();
+  var x = e.target.dataset.pk;
+  var comment =
+    '<div class="comment-reply"><form method="post"><input type="hidden" name="csrfmiddlewaretoken" value="' +
+    getCookieValue("csrftoken") +
+    '" /> <input type="hidden" name="span-answer" value="' +
+    x +
+    '" /><div class="form-group row"><textarea placeholder="Ingresa tu comentario..." class="col-sm-12" name="body" id="body" rows="3" required></textarea></div><button type="submit" class="btn btn-warning">Enviar</button></form></div>';
+  var parent = e.target.parentNode.parentNode.parentNode.parentNode.parentNode;
+  var elem = parent.children[2];
+
+  elem.insertAdjacentHTML("beforeend", comment);
 }
 
 function removeObjective(e) {
